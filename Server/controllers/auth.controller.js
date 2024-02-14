06cb77da2eab7a615e2179f46d9fbe59a17f3238
aditya-goyal3302 <agoyal2505.ca21@chitkara.edu.in>
2024-02-14 18:38:21 +0530

    correction of codes and errors

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -2,8 +2,10 @@ const {auth_service} = require('../services/index');
 
 exports.signup = async (req, res) => {
     try {
+        if(!req.body.first_name || !req.body.last_name || !req.body.username || !req.body.email || !req.body.password){
+            throw Object.assign(new Error("Invalid_input"), { code: 400 });
+        }
         const result = await auth_service.signup(req);
-        
          return res.status(201).send(result);
     } catch (error) {
         console.log('error_in_signup: ', error);
