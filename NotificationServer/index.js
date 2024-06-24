56047163736b7c27ee81bed9398686ca44bbce15
ADITYA GOYAL <agdoie3302@gmail.com>
2024-06-24 23:50:17 +0530

    .. correction

--- NotificationServer/index.js
+++ NotificationServer/index.js
@@ -1,14 +1,15 @@
 require("dotenv").config()
 const express = require("express");
 const app = express();
-app.use(require('cors')())
 const http = require("http");
+const cors = require("cors");
+app.use(cors());
 const server = http.createServer(app)
 const {socket} =require("./config").
 socket(server);
 // parse application/json
-app.use(express.json({ extended: true }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '5mb', extended: true }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
 // Connect to DB
 require("./config/mongo_db").connectDB();
@@ -16,6 +17,12 @@ require("./config/mongo_db").connectDB();
 
 app.use('/',require('./routes'))
 
+app.use((req, res) => {
+    // console.log('err: ', req);
+    // if (error instanceof MulterError)
+      // return res.status(400).send("File size Error")
+    res.status(404).send({ e: "Page not found" });
+  });
 
 server.listen(process.env.PORT, ()=>
     console.log(`Notification Server is up and running on port ${process.env.PORT}`)
